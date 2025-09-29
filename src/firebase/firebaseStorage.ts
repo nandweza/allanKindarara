8b79673144904cc702898300d4d08556635a0ae9
nandweza <allannandweza@gmail.com>
2025-09-28 22:39:50 -0600

    created next.js app

--- src/firebase/firebaseStorage.ts
+++
@@ -1,9 +0,0 @@
-// firebaseStorage.s
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
-import { firebaseConfig } from './firebaseConfig';
-
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-
-export default storage;
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './index';
-
-const port: number = 5000;
-const hostname: string = 'http://127.0.0.1';
-
-app.listen(port, () => {
-    console.log(`Server running on ${hostname}:${port}`);
-});
